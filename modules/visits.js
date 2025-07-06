94216e7ae855aaac7b8258a24136d4b6e0666ea2
Lincoln Touw <149920595+lincolntouw@users.noreply.github.com>
2025-07-06 12:45:24 -0400

    Update visits.js

--- modules/visits.js
+++ modules/visits.js
@@ -2,6 +2,7 @@ var proxy = 'https://script.google.com/macros/s/AKfycbxwVCgNXp4MmhOGmiYuG2ZYz-Dj
 async function getGames(userId, cursor) {
   const allGames = [];         
   const list = await(await fetch( proxy + encodeURIComponent(`https://games.roblox.com/v2/users/${userId}/games?sortOrder=Asc&limit=50${cursor != undefined ? `&cursor={cursor}` : ""}`), { redirect: 'follow', } )).json();
+  console.log(list);
   list.data.forEach((e, i) => allGames.push(e));                         
   if (list.nextPageCursor) {
     const [next, nextCursor] = getGames(userId, list.nextPageCursor);          
